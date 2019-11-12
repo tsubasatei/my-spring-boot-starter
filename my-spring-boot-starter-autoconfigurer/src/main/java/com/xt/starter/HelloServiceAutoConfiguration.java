41b8246939121c905d81ec33ff966d0544cd1648
tsubasatei <tsubasa_337283708@qq.com>
2019-11-12 08:28:25 +0800

    Initial commit

---
+++ my-spring-boot-starter-autoconfigurer/src/main/java/com/xt/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.xt.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication  // web 应用才生效
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+
+    @Bean
+    public HelloService helloService() {
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+}
